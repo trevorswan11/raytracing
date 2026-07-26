ef711ed44e870c76fff32e9c32bce9b06657c529
trevorswan11 <tcs94@case.edu>
2026-07-26 14:25:23 -0400

    feat(4): Add camera/ray behavior to launcher

--- src/raytracer/launcher.cc
+++ src/raytracer/launcher.cc
@@ -1,5 +1,6 @@
 #include "raytracer/launcher.hh"
 
+#include <algorithm>
 #include <fstream>
 #include <ios>
 
@@ -15,6 +16,7 @@
 
 #include "raytracer/color.hh"
 #include "raytracer/progress.hh"
+#include "raytracer/ray.hh"
 #include "raytracer/vec.hh"
 
 namespace raytracer {
@@ -38,9 +40,45 @@ launcher::launcher(i32 argc, char** argv) : args_{argv, static_cast<usize>(argc)
     }
 }
 
+namespace {
+
+[[nodiscard]] auto ray_color(const ray& r) -> color {
+    const auto unit_direction{r.direction().unit()};
+    const auto a{0.5 * (unit_direction.y() + 1.0)};
+    return (1.0 - a) * color{1.0} + a * color{0.5, 0.7, 1.0};
+}
+
+} // namespace
+
 auto launcher::launch() -> stdx::result<void, i32> {
-    const i32 image_width{256}, image_height{256};
-    progress  bar{image_height};
+    // Image
+    constexpr auto aspect_ratio{16.0 / 9.0};
+    const i32      image_width{400};
+    const auto     image_height{std::max(1, static_cast<i32>(image_width / aspect_ratio))};
+
+    // Camera
+    const auto focal_length{1.0};
+    const auto viewport_height{2.0};
+
+    // Don't use aspect_ratio here since the true ratio may be different with truncating
+    const auto viewport_width{viewport_height * (static_cast<f64>(image_width) / image_height)};
+    point3     camera_center{};
+
+    // Calculate vectors across the horizontal and down the vertical edges
+    const vec3 viewport_u{viewport_width, 0, 0};
+    const vec3 viewport_v{0, -viewport_height, 0};
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel
+    const auto pixel_delta_u{viewport_u / image_width};
+    const auto pixel_delta_v{viewport_v / image_height};
+
+    // Calculate the location of the upper left pixel
+    const auto viewport_upper_left{camera_center - vec3{0, 0, focal_length} - viewport_u / 2 -
+                                   viewport_v / 2};
+    const auto pixel00_loc{viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v)};
+
+    // Render
+    progress bar{image_height};
 
     fmt::println(outfile_, "P3");
     fmt::println(outfile_, "{} {}", image_width, image_height);
@@ -49,9 +87,11 @@ auto launcher::launch() -> stdx::result<void, i32> {
     for (i32 j{0}; j < image_height; ++j) {
         bar.update(1);
         for (i32 i{0}; i < image_width; ++i) {
-            const vec3 pixel_color{static_cast<f64>(i) / (image_width - 1),
-                                   static_cast<f64>(j) / (image_height - 1),
-                                   0};
+            const auto pixel_center{pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v)};
+            const auto ray_direction{pixel_center - camera_center};
+            const ray  r{camera_center, ray_direction};
+
+            const auto pixel_color{ray_color(r)};
             write_color(outfile_, pixel_color);
         }
     }
