9ba188c10adc1ef7a13d8c98f8d84b18447d34a7
trevorswan11 <tcs94@case.edu>
2026-07-26 11:05:04 -0400

    feat: Add remaining helpers to vector

--- include/ray/vec.hh
+++ include/ray/vec.hh
@@ -7,6 +7,9 @@
 #include <functional>
 #include <ranges>
 
+#include <fmt/base.h>
+#include <fmt/ranges.h>
+#include <stdx/assert.hh>
 #include <stdx/iterator.hh>
 #include <stdx/types.hh>
 
@@ -14,16 +17,16 @@ namespace ray {
 
 namespace detail {
 
-template <std::floating_point F, u32 N>
+template <std::floating_point F, usize N>
     requires(N > 1)
-class vec_t {
+class vec {
   public:
     using data_t = std::array<F, N>;
     MAKE_UNALIASED_ITERATOR(data_t, data_)
 
   public:
-    constexpr vec_t() noexcept { data_.fill(static_cast<F>(0.0)); };
-    constexpr vec_t(data_t data) noexcept : data_{std::move(data)} {}
+    constexpr vec() noexcept { data_.fill(static_cast<F>(0.0)); };
+    constexpr vec(data_t data) noexcept : data_{std::move(data)} {}
 
     [[nodiscard]] auto x() const noexcept -> F { return data_[0]; }
     [[nodiscard]] auto y() const noexcept -> F { return data_[1]; }
@@ -39,29 +42,78 @@ class vec_t {
         return data_[3];
     }
 
-    [[nodiscard]] auto operator-() const noexcept -> vec_t {
-        vec_t new_vec;
+    [[nodiscard]] auto data(this auto&& self) noexcept -> auto* { return self.data_.data(); }
+    [[nodiscard]] auto operator[](this auto&& self, usize idx) noexcept -> decltype(auto) {
+        ASSERT(idx < N, "vec index out of range");
+        return self.data_[idx];
+    }
+
+    [[nodiscard]] auto operator-() const noexcept -> vec {
+        vec new_vec;
         std::ranges::transform(data_, new_vec.begin(), std::negate{});
         return new_vec;
     }
 
-    [[nodiscard]] auto operator+=(const vec_t& v) noexcept -> vec_t& {
+    [[nodiscard]] auto operator+=(const vec& v) noexcept -> vec& {
         for (auto& [x, vx] : data_ | std::views::zip(v)) { x += vx; }
         return *this;
     }
 
-    [[nodiscard]] auto operator*=(F t) noexcept -> vec_t& {
+    [[nodiscard]] auto operator*=(F t) noexcept -> vec& {
         std::ranges::for_each(data_, [t](F& x) { x *= t; });
         return *this;
     }
 
-    [[nodiscard]] auto operator/=(F t) noexcept -> vec_t& { return *this *= 1 / t; }
+    [[nodiscard]] auto operator/=(F t) noexcept -> vec& { return *this *= 1 / t; }
 
+    [[nodiscard]] auto unit() const noexcept -> vec { return *this / length(); }
     [[nodiscard]] auto length() const noexcept -> F { return std::sqrt(length_squared()); }
-    [[nodiscard]] auto length_squared() const noexcept -> F {
-        return std::ranges::fold_left(data_ | std::views::transform([](F x) { return x * x; }),
-                                      static_cast<F>(0.0),
-                                      std::plus{});
+    [[nodiscard]] auto length_squared() const noexcept -> F { return dot_with(*this); }
+    [[nodiscard]] auto dot(const vec& v) const noexcept -> F { return dot_with(v); }
+
+    [[nodiscard]] auto cross(const vec& v) const noexcept -> vec
+        requires(N == 3)
+    {
+        return vec{data_[1] * v[2] - data_[2] * v[1],
+                   data_[2] * v[0] - data_[0] * v[2],
+                   data_[0] * v[1] - data_[1] * v[0]};
+    }
+
+    [[nodiscard]] friend auto operator+(const vec& u, const vec& v) noexcept -> vec {
+        auto data{u | std::views::zip(v) | std::views::transform(std::plus{}) |
+                  std::ranges::to<data_t>()};
+        return vec{std::move(data)};
+    }
+
+    [[nodiscard]] friend auto operator-(const vec& u, const vec& v) noexcept -> vec {
+        auto data{u | std::views::zip(v) | std::views::transform(std::minus{}) |
+                  std::ranges::to<data_t>()};
+        return vec{std::move(data)};
+    }
+
+    [[nodiscard]] friend auto operator*(const vec& u, const vec& v) noexcept -> vec {
+        auto data{u | std::views::zip(v) | std::views::transform(std::multiplies{}) |
+                  std::ranges::to<data_t>()};
+        return vec{std::move(data)};
+    }
+
+    [[nodiscard]] friend auto operator*(F t, const vec& v) noexcept -> vec {
+        auto data{v | std::views::transform([t](F x) { return x * t; }) |
+                  std::ranges::to<data_t>()};
+        return vec{std::move(data)};
+    }
+
+    [[nodiscard]] friend auto operator*(const vec& v, F t) noexcept -> vec { return t * v; }
+
+    [[nodiscard]] friend auto operator/(const vec& v, F t) noexcept -> vec { return (1 / t) * v; }
+
+  private:
+    [[nodiscard]] auto dot_with(const vec& v) const noexcept -> F {
+        return std::ranges::fold_left(
+            data_ | std::views::zip(v) |
+                std::views::transform([](auto p) { return p.first * p.second; }),
+            static_cast<F>(0.0),
+            std::plus{});
     }
 
   private:
@@ -70,8 +122,19 @@ class vec_t {
 
 } // namespace detail
 
-using vec3   = detail::vec_t<f64, 3>;
+using vec3   = detail::vec<f64, 3>;
 using point3 = vec3;
 using color  = vec3;
 
 } // namespace ray
+
+template <std::floating_point F, usize N> struct fmt::formatter<ray::detail::vec<F, N>> {
+    template <typename FormatContext> constexpr auto parse(FormatContext& ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(const ray::detail::vec<F, N>& vec, FormatContext& ctx) const {
+        return fmt::format_to(ctx.out(), "{}", fmt::join(vec, " "));
+    }
+};
